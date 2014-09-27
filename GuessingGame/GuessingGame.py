d5eb828c51815b4ad716223e510aff0a7659c2aa
Derek Corrigan <derek.joseph.corrigan@gmail.com>
2014-09-27 10:36:05 -0700

    completed the game. very simple program with one while loop and a couple conditional statements

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -1,4 +1,6 @@
 __author__ = 'Derek'
+# the guessing game program. Computer generates a random number and
+# the player has to try to guess it in as few guesses as possible
 
 from random import randrange
 
@@ -6,5 +8,25 @@ from random import randrange
 # and the extra call can effect performance (not that I really care
 # for this program!)
 randomNum = randrange(1, 100)
+running = True
+count = 0
 
-print randomNum
+print "This is the guessing game!"
+
+while running:
+    guess = int(raw_input('Enter an integer between 1 and 100: '))
+
+    if guess == randomNum:
+        print 'Congratulations, you guessed it.'
+        count += 1
+        running = False
+    elif guess < randomNum:
+        print 'Higher!'
+        count += 1
+    else:
+        print 'Lower!'
+        count += 1
+else:
+    print '\nThe game is over. You got the right answer in {} guess(es)\n'.format(count)
+
+print 'Done'
